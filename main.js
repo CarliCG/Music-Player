12d27ceb981d721ccb1cbc89b1fd406391209b05
nsandoya <natsandoya@gmail.com>
2024-02-04 18:58:28 -0500

    Búsqueda: se muestran resultados en lista general | Pendiente: Mostrar por defecto toda la biblioteca en la lista general

--- main.js
+++ main.js
@@ -6,18 +6,11 @@ const listaMusica = ["Dare you to move", "This is home", "Somewhere only we know
 let buscador = document.getElementById('input-buscador')
                         //evento   fx
 buscador.addEventListener('input', function() {
-    /* console.log(this.value); */
     cancion = this.value
     /* Regex */
     let expresion = new RegExp(cancion, "i");
      /* Función para comparar input con array */
     const cancionResultado = comparar(cancion, expresion);
-
-    /* if(cancionResultado.length == 0){
-        alert("No hay coincidencias")
-    } */
-    console.log(cancionResultado)
-    /* console.log(cancionResultado) */
 });
 
 /* 2. Comparar con la lista */
@@ -25,21 +18,12 @@ function comparar(input, expresion){
     let resultadoCancion = listaMusica.filter(
         song => expresion.test(song)
     );
-    /* const resultadoCancion = listaMusica.filter((cancion) => {
-        if(cancion == input){ 
-            return(cancion)
-        }
-    }
-    ) */
-    /* return(resultadoCancion); */
     /* 3. Mostrar resultados */
     let contenedorBusqueda= document.getElementById("lista-general");
-
-    /* console.log(contenedorBusqueda) */
-     
+   
+    contenedorBusqueda.innerHTML = "";
     resultadoCancion.forEach(
         song => {
-            contenedorBusqueda.innerHTML = "";
             contenedorBusqueda.innerHTML += `<li><h3>${song}</h3><i class="fa-solid fa-plus"></i><i class="fa-regular fa-heart"></i>
             </li>`
         }
