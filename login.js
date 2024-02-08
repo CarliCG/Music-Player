e16229159d513b7d06135681f844f084fbbbfef8
nsandoya <natsandoya@gmail.com>
2024-02-08 16:30:39 -0500

    Login -> JS: Se implementó el código necesario para hacer login | CSS: Se hicieron cambios de estilo || Reproductor: Sin novedades significativas

---
+++ login.js
@@ -0,0 +1,39 @@
+const users = [
+    {
+        name: "Ana",
+        password: "ana"
+    },
+    {
+        name: "Carli",
+        password: "carli"
+    },
+    {
+        name: "Nataly",
+        password: "nat"
+    },
+    {
+        name: "Nataly",
+        password: "nathaly"
+    }
+]
+
+const userInput = document.getElementById("user")
+const password = document.getElementById("password")
+const loginBtn = document.getElementById("login")
+
+
+loginBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    /* Acá se verifica si los inputs ingresados coinciden con los registros de usuario */
+    const user = users.find((user) => user.name == userInput.value && user.password == password.value)
+    if(user){
+        console.log(user);
+        localStorage.setItem('isLogged', true) // Se crea una variable en el local storage, y se le asigna 'true' como valor
+        localStorage.setItem('userName', userInput.value) // Se crea una variable en el local storage, y se le asigna el usuario como valor
+        window.location.href = "./reproductor.html"
+    }else alert("Usuario/Contraseña incorrecto(s)")
+})
+/* En caso de estar ya loggeado, se redirige siempre al usuario al Home */
+if(localStorage.getItem('isLogged')){
+    window.location.href = "./reproductor.html"
+}
--- main.js
+++ main.js
@@ -1,42 +1,74 @@
 console.log("funciona");
 
-const listaMusica = ["Dare you to move", "This is home", "Somewhere only we know", "This is your life", "Stay"]
+const listaMusica = ["Let your faith...", "Cry", "Dare you to move", "This is home", "Somewhere only we know", "This is your life", "Stay"]
+
+
+userName = localStorage.getItem('userName')
+console.log(userName)
+userTitle = document.getElementById('userTitle')
+userTitle.innerText = userName
 
 /* Mostrar por defecto todas las canciones de la biblioteca */
 let contenedorBusqueda= document.getElementById("lista-general");
 listaMusica.forEach(
     song => {
-        contenedorBusqueda.innerHTML += `<li><h3>${song}</h3><i class="fa-solid fa-plus"></i><i class="fa-regular fa-heart"></i>
+        contenedorBusqueda.innerHTML += `<li><h3 class="cancion">${song}</h3><i class="fa-solid fa-plus"></i><i class="fa-regular fa-heart"></i>
         </li>`
     }
     );
     
-/* 1. Obtener value de input-buscador y ejecutar fx según evento*/
+    /* 1. Obtener value de input-buscador y ejecutar fx según evento*/
 let buscador = document.getElementById('input-buscador')
-                        //evento   fx
+    //evento   fx
 buscador.addEventListener('input', function() {
+    
     cancion = this.value
     /* Regex */
     let expresion = new RegExp(cancion, "i");
-     /* Función para comparar input con array */
-    const cancionResultado = comparar(cancion, expresion);
+    /* Función para comparar input con array */
+    const cancionResultado = comparar(expresion);
+    
+    let tituloBusqueda= document.getElementById("biblio-titulo");
+    if(!this.value){
+        tituloBusqueda.textContent = "Biblioteca General";
+    }
 });
 
+
 /* 2. Comparar con la lista */
-function comparar(input, expresion){
+function comparar(expresion){
     let resultadoCancion = listaMusica.filter(
-        song => expresion.test(song)
-    );
+        // Song: canción de c/iteración
+        song => expresion.test(song) // Hay alguna coincidencia entre 'song' y la regex?
+        // Si la r// es True, el return (implícito) será la presente canción
+        );
     /* 3. Mostrar resultados */
+    let tituloBusqueda= document.getElementById("biblio-titulo");
+    tituloBusqueda.textContent = "Resultados de la búsqueda:";
+
     let contenedorBusqueda= document.getElementById("lista-general");
-   
     contenedorBusqueda.innerHTML = "";
     resultadoCancion.forEach(
         song => {
             contenedorBusqueda.innerHTML += `<li><h3>${song}</h3><i class="fa-solid fa-plus"></i><i class="fa-regular fa-heart"></i>
             </li>`
         }
-     
     );
 }
 
+const logOut = document.getElementById("log-out")
+logOut.addEventListener('click',() => {
+    localStorage.removeItem('isLogged')
+    localStorage.removeItem('userName')
+    window.location.href = "./login.html"   
+})
+
+// Si no estamos logeados, no vamos a poder entrar a la landing page
+if(!localStorage.getItem('isLogged')){
+    window.location.href = "./login.html"
+}/* else{
+    alert("Bienvenido!")
+}  */
+
+
+/* Clases */
