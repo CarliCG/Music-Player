32e58c551145b60df61e3cb88495750bc769360e
nsandoya <natsandoya@gmail.com>
2024-02-08 16:36:31 -0500

    Reproductor -> Se implementaron las nuevas clases y métodos en en .js, además de los cambios correspondientes en el .html

--- main.js
+++ main.js
@@ -1,74 +1,180 @@
-console.log("funciona");
+class Song{
+    constructor({name, artist, duration, album, gender, year, isFav = false, onPlayList = false, urlCover, urlSong}){
+        this.name = name;
+        this.artist = artist;
+        this.duration = duration;
+        this.album = album;
+        this.gender = gender;
+        this.year = year;
+        this.isFav = isFav;
+        this.onPlayList = onPlayList;
 
-const listaMusica = ["Let your faith...", "Cry", "Dare you to move", "This is home", "Somewhere only we know", "This is your life", "Stay"]
-
-
-userName = localStorage.getItem('userName')
-console.log(userName)
-userTitle = document.getElementById('userTitle')
-userTitle.innerText = userName
-
-/* Mostrar por defecto todas las canciones de la biblioteca */
-let contenedorBusqueda= document.getElementById("lista-general");
-listaMusica.forEach(
-    song => {
-        contenedorBusqueda.innerHTML += `<li><h3 class="cancion">${song}</h3><i class="fa-solid fa-plus"></i><i class="fa-regular fa-heart"></i>
-        </li>`
+        this.urlCover = urlCover;
+        this.urlSong = urlSong;
     }
-    );
-    
-    /* 1. Obtener value de input-buscador y ejecutar fx según evento*/
-let buscador = document.getElementById('input-buscador')
-    //evento   fx
-buscador.addEventListener('input', function() {
-    
-    cancion = this.value
-    /* Regex */
-    let expresion = new RegExp(cancion, "i");
-    /* Función para comparar input con array */
-    const cancionResultado = comparar(expresion);
-    
-    let tituloBusqueda= document.getElementById("biblio-titulo");
-    if(!this.value){
-        tituloBusqueda.textContent = "Biblioteca General";
+
+    getName(){
+        return(this.name);
+    };
+
+    getArtist(){
+        return(this.artist);
     }
-});
-
-
-/* 2. Comparar con la lista */
-function comparar(expresion){
-    let resultadoCancion = listaMusica.filter(
-        // Song: canción de c/iteración
-        song => expresion.test(song) // Hay alguna coincidencia entre 'song' y la regex?
-        // Si la r// es True, el return (implícito) será la presente canción
-        );
-    /* 3. Mostrar resultados */
-    let tituloBusqueda= document.getElementById("biblio-titulo");
-    tituloBusqueda.textContent = "Resultados de la búsqueda:";
-
-    let contenedorBusqueda= document.getElementById("lista-general");
-    contenedorBusqueda.innerHTML = "";
-    resultadoCancion.forEach(
-        song => {
-            contenedorBusqueda.innerHTML += `<li><h3>${song}</h3><i class="fa-solid fa-plus"></i><i class="fa-regular fa-heart"></i>
-            </li>`
-        }
-    );
 }
 
-const logOut = document.getElementById("log-out")
-logOut.addEventListener('click',() => {
-    localStorage.removeItem('isLogged')
-    localStorage.removeItem('userName')
-    window.location.href = "./login.html"   
+class Playlist{
+    constructor({listName, songs = []}){
+        this.listName = listName;
+        this.songs = songs;
+    }
+
+    addSong(...song){
+        this.songs.push(...song)
+    }
+
+    removeSong(song){
+        const index = this.songs.indexOf(song);
+        if (index === -1) return // Esto indica que no encontró la canción
+        this.songs.splice(index, 1) // Si existe la canción, se borra usando su posición(index), solo ese elemento ('1')
+        this.renderList(idContainer); // Por c/vez que eliminemos una nueva canción a la lista, el html que muestra la lista se actualiza, y se muestra todo menos la canción borrada
+    }
+
+    renderList(lista = this.songs){
+        let contenedor= document.getElementById("lista-general");
+        /* Muestra todas las canciones de la lista en cuestión */
+        contenedor.innerHTML = "";
+        lista.forEach(
+            song => {
+                contenedor.innerHTML += `<li><h3 class="cancion">${song.name}</h3><i class="fa-solid fa-plus"></i><i class="fa-regular fa-heart"></i>
+                </li>`  // Aquí hará falta validar si song.isFav = true (y así), varíen los botones de favorito, playlist...
+            });
+    };
+
+    renderListBtn(btnValue, lista = this.songs){
+        let btn = new RegExp(btnValue, "i");
+        let contenedor2= document.getElementById("lista-general-2");
+
+        console.log(contenedor2)
+
+        // Acá hay que añadir un event listener, para que este código entre en acción cuando se presionen los respectivos botones
+
+        if(btn.test(this.listName)){
+        /* Muestra todas las canciones de la lista en cuestión */
+            contenedor2.innerHTML = "";
+            lista.forEach(
+                song => {
+                    contenedor2.innerHTML += `<li><h3 class="cancion">${song.name}</h3><i class="fa-solid fa-plus"></i><i class="fa-regular fa-heart"></i>
+                    </li>`
+                });  // Aquí hará falta validar si song.isFav = true (y así), varíen los botones de favorito, playlist...
+        }
+    }
+
+    searchBy(met, listaCanciones = this.songs){
+        const buscador = document.getElementById("input-buscador")
+        listaCanciones = this.songs;
+
+        buscar();
+
+        function buscar (){
+                                    //evento   fx
+            buscador.addEventListener('input', function() {
+                let input = this.value;
+                /* Regex */
+                let expresion = new RegExp(input, "i");
+                /* Función para comparar input con array */
+                const inputResultado = comparar(listaCanciones, expresion);
+                /* console.log(inputResultado) */
+    
+                let tituloBusqueda= document.getElementById("biblio-titulo");
+                if(!this.value){
+                    tituloBusqueda.textContent = "Biblioteca General";
+                }else{
+                    tituloBusqueda.textContent = "Resultados de la búsqueda:";
+                }
+                met(inputResultado)
+                return(inputResultado)
+            });
+        }
+
+        function comparar(lista, expresion){
+            /* console.log(lista); */
+            let resultadoCancion = lista.filter(
+                // Song: canción de c/iteración
+                song => expresion.test(song.name) || expresion.test(song.artist) || expresion.test(song.album) || expresion.test(song.gender)// Hay alguna coincidencia entre 'song' y la regex?
+                // Si la r// es True, el return (implícito) será la presente canción
+            );
+            return(resultadoCancion)
+        
+        }
+    }
+}
+
+class MusicPlayer{
+    constructor({currentPlayList}){
+        this.currentPlayList = currentPlayList;
+    }
+    /* Los métodos a crear acá se comunicarán con el HTMLAudioElement. 
+    Cuando se de click en el music player, estos métodos invocarán a su vez los métodos propios del HTMLAudioElement, y se podrán reproducir las canciones*/
+
+    /* Al parecer, el HTMLAudioElement solo admite 1 canción por vez. En tal caso, es necesario guardar la canción a la que le demos play en una variable, y eso enviarlo al objeto audio, y luego disparar la fx play */
+}
+
+
+/* Crear lista de nuevas canciones */
+const songs = [
+    new Song({
+    name: "Stay",
+    artist: "Robbie Seay Band",
+    duration: "05:00",
+    album: "Give yourself away",
+    gender: "Christian rock",
+    year: 2018,
+}),
+    new Song({
+    name: "This is home",
+    artist: "Switchfoot",
+    duration: "03:00",
+    album: "Chronicles of Narnia",
+    gender: "Christian rock",
+    year: 2008,
+}),
+    new Song({
+    name: "Somewhere only we know",
+    artist: "Keane",
+    duration: "02:50",
+    album: "Hopes and fears",
+    gender: "Rock",
+    year: 2002,
+})]
+
+console.log(songs)
+
+/* Crear nueva canción */
+const letyourfaith = new Song({
+    name: "Let your faith be not alone",
+    artist: "Robbie Seay Band",
+    duration: "06:00",
+    album: "Miracles",
+    gender: "Christian rock",
+    year: 2020,
+})
+/* Crear nuevos playlists */
+const biblioGeneral = new Playlist({
+    listName: "Biblioteca General"
+})
+const myPlaylist = new Playlist({
+    listName: "My PlayList"
 })
 
-// Si no estamos logeados, no vamos a poder entrar a la landing page
-if(!localStorage.getItem('isLogged')){
-    window.location.href = "./login.html"
-}/* else{
-    alert("Bienvenido!")
-}  */
+/* Agregar canciones al playlist */
+biblioGeneral.addSong(...songs)
+console.log(biblioGeneral.songs)
+/* Mostrar playlist en su container */
+biblioGeneral.renderList()
+/* Ejecutar la búsqueda (lado izq) */
+biblioGeneral.searchBy(biblioGeneral.renderList)
 
-
-/* Clases */
+/* Agregar canción a playlistt */
+myPlaylist.addSong(letyourfaith),
+/* Mostrar playlist en su container */
+myPlaylist.renderListBtn("My PlayList")
